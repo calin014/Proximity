8f6e259facfd22bf37ed6b6144b050a12759af40
calin <calin014@gmail.com>
2016-10-21 14:36:54 +0300

    Added Rx

--- app/src/main/java/calin/proximity/core/GamePlay.kt
+++ app/src/main/java/calin/proximity/core/GamePlay.kt
@@ -1,23 +1,11 @@
 package calin.proximity.core
 
+import rx.Observable
+
 /**
  * Created by calin on 10/17/2016.
  */
 
-interface GamePlay {
-    interface GameObserver {
-        fun playerIsInDefusingArea(bomb: ProximityBomb)
-
-        fun playerIsInDetonationArea(bomb: ProximityBomb)
-
-        fun playerHasDied(bomb: ProximityBomb)
-    }
-
-    fun createGame(player: Player, rules: Rules, observer: GameObserver)
-
-    fun movePlayer(newLocation: Location)
-
-    fun placeBomb(bomb: ProximityBomb)
-
-    fun defuseBomb(bomb: ProximityBomb)
+class GamePlay(game: ProximityGame) {
+    fun setupCreateGame(o:Observable<Unit>) {}
 }
